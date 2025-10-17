83c76d6dd3f4e458ce9cd7b9d0aa1cdda35ef30f
Salman Yahya <salman.yahya@onenetwork.se>
2025-10-17 19:18:55 +0200

    mainupdae

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"+6IRWofNDVAyq6dVNyg0NojEd+GqijCnVa2ludiiq+0=\"}"
---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  // Remove any module.exports if using ES modules
+}
+
+module.exports = nextConfig
